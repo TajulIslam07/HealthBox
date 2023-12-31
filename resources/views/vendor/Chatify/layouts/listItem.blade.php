84e35029f3d2b095a8853dce37a635b37904c01f
Tawfiq Elahi <tawfiqelahi9@gmail.com>
2023-12-31 14:06:55 +0600

    chat

--- resources/views/vendor/Chatify/layouts/listItem.blade.php
+++ resources/views/vendor/Chatify/layouts/listItem.blade.php
@@ -1,4 +1,4 @@
-{{-- -------------------- Saved Messages -------------------- --}}
+ {{-- -------------------- Saved Messages -------------------- --}}
 @if($get == 'saved')
     <table class="messenger-list-item" data-contact="{{ Auth::user()->id }}">
         <tr data-action="0">
